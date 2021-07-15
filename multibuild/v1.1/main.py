4367492da9a513c6944ee99516213b3bbcb7726e
Paradeisios <p.boulakis@student.maastrichtuniversity.nl>
2021-07-15 11:36:10 +0300

    Update main.py

--- multibuild/v1.1/main.py
+++ multibuild/v1.1/main.py
@@ -5,7 +5,6 @@ from utils.tutorials import *
 movie_fname = select_movie()  #select movie
 log_data = demographics()  #get demographics
 
-tutorial_phase = True
 
 specify_experiment_phase(movie_fname,log_data)
 
